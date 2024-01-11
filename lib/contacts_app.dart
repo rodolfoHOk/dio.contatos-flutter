fe5724e8b6e376f1305e51c72cb52dde7d83783d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-10 21:22:34 -0300

    styles: initial config themes

--- lib/contacts_app.dart
+++ lib/contacts_app.dart
@@ -9,8 +9,17 @@ class ContactsApp extends StatelessWidget {
     return MaterialApp(
       title: 'Contacts App',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          primary: Colors.indigo,
+          onPrimary: Colors.white,
+          inversePrimary: Colors.indigo,
+        ),
         useMaterial3: true,
       ),
       home: const MainPage(title: 'Contatos'),
